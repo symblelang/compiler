9a384ec0abdae14468e5368965e1f82492a55710
aierus <clarkui@bc.edu>
2023-05-15 11:53:11 -0400

    fix test script

--- tests/testscript.sh
+++ tests/testscript.sh
@@ -4,7 +4,7 @@ source progress_bar.sh
 
 make clean --directory=../
 
-tasks_in_total=37
+tasks_in_total=17
 for current_task in $(seq $tasks_in_total) 
     do
     sleep 0.1
@@ -12,66 +12,70 @@ for current_task in $(seq $tasks_in_total)
 done
 
 make --directory=../
+echo "✅ Compilation Complete"
+
 cd ../utils/
 doxygen
+if [ $? -ne 0 ]; then
+    echo "❌ Error Running Doxygen"
+else 
+    echo "✅ Doxygen Documentation Generated"
+fi
 
 cd ../tests/
 
-# Unit Tests
-retVal = $?
+# Symble Unit Tests
 
+echo -e "\n"
 echo Running Comment Test...
 ../bin/symble comment_test.sy
-if [ $retVal -ne 0 ]; then
-    echo "Error Running Comment Test"
+if [ $? -ne 0 ]; then
+    echo "❌ Error Running Comment Test"
 else 
-    echo "Comment Test Complete!"
+    echo "✅ Comment Test Complete!"
 fi
 
+echo -e "\n"
 echo Running Expr Test...
 ../bin/symble expr_test.sy
-if [ $retVal -ne 0 ]; then
-    echo "Error Running expr Test"
+if [ $? -ne 0 ]; then
+    echo "❌ Error Running expr Test"
 else 
-    echo "expr Test Complete!"
+    echo "✅ expr Test Complete!"
 fi
 
+echo -e "\n"
 echo Running function Test...
 ../bin/symble function_test.sy
-if [ $retVal -ne 0 ]; then
-    echo "Error Running function Test"
+if [ $? -ne 0 ]; then
+    echo "❌ Error Running function Test"
 else 
-    echo "function Test Complete!"
+    echo "✅ function Test Complete!"
 fi
 
+echo -e "\n"
 echo Running if_statement Test...
 ../bin/symble if_statement.sy
-if [ $retVal -ne 0 ]; then
-    echo "Error Running if_statement Test"
+if [ $? -ne 0 ]; then
+    echo "❌ Error Running if_statement Test"
 else 
-    echo "if_statement Test Complete!"
+    echo "✅ if_statement Test Complete!"
 fi
 
-echo Running symbol_table Test...
-../bin/symble symbol_table_test.sy
-if [ $retVal -ne 0 ]; then
-    echo "Error Running symbol_table Test"
-else 
-    echo "symbol_table Test Complete!"
-fi
-
+echo -e "\n"
 echo Running while_loop Test...
 ../bin/symble while_loop.sy
-if [ $retVal -ne 0 ]; then
-    echo "Error Running while_loop Test"
+if [ $? -ne 0 ]; then
+    echo "❌ Error Running while_loop Test"
 else 
-    echo "while_loop Test Complete!"
+    echo "✅ while_loop Test Complete!"
 fi
 
+echo -e "\n"
 echo Running sieve Test...
-../bin/symble while_loop.sy
-if [ $retVal -ne 0 ]; then
-    echo "Error Running sieve Test"
+../bin/symble sieve.sy
+if [ $? -ne 0 ]; then
+    echo "❌ Error Running sieve Test"
 else 
-    echo "sieve Test Complete!"
+    echo "✅ sieve Test Complete!"
 fi
